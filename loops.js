ad885d7642da8159eb8e7188eed742c6b416e034
Brittany Nieves <neopetlover912002@yahoo.com>
2018-05-11 22:46:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function forLoop(array){
   for (var i = 0; i < 25; i++){
-    if (i == 1){
+    if (i === 1){
       array.push('I am a strange loop.');
     }
     else {
-      array.push('I am ${i} strange loops.')
+      array.push('I am ${i} strange loops.');
     }
-    return array
+    return array;
   }
   
 }
